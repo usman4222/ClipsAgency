24245b7cf9e784377d13188aa1fe2b3ec0414fbf
usman4222 <usman4243ch@gmail.com>
2025-03-01 18:39:13 +0500

    add animation on text

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,19 +13,3 @@ module.exports = {
   },
   plugins: [],
 };
-
-
-
-
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        polysans: ["Polysans median", "sans-serif"],
-      },
-    },
-  },
-  plugins: [],
-};
